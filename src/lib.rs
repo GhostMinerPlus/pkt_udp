1b1c02df3d3faa6afd9e2d55519c36d5be2f6386
ghostminer <1696415745@qq.com>
2023-11-29 23:30:04 +0800

    # time out Remove timeout

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,12 @@
 use std::{
     collections::BTreeSet,
     io::{self, Error, ErrorKind},
-    net::{ToSocketAddrs, UdpSocket},
-    time::Duration,
+    net::{ToSocketAddrs, UdpSocket}
 };
 
 const MAX_LOST_SIZE: usize = 10;
 const DATA_SZ: usize = 512;
 const FRAME_SZ: usize = 8 + 4 + 2 + DATA_SZ;
-const READ_TIMEOUT: u32 = 460_000;
 
 struct PktState {
     pkt_id: u64,
@@ -67,7 +65,6 @@ impl PktServer {
         let (_, addr) = self.sock.recv_from(&mut buf)?;
 
         let sock = UdpSocket::bind("0.0.0.0:0")?;
-        sock.set_read_timeout(Some(Duration::new(0, READ_TIMEOUT)))?;
         sock.connect(addr)?;
         sock.send(&buf)?;
         Ok(PktConn { sock, pkt_id: 0 })
@@ -86,7 +83,6 @@ impl PktConn {
     {
         let mut buf = [0; FRAME_SZ];
         let sock = UdpSocket::bind("0.0.0.0:0")?;
-        sock.set_read_timeout(Some(Duration::new(0, READ_TIMEOUT)))?;
         sock.send_to(&buf, addr)?;
         let (_, addr) = sock.recv_from(&mut buf)?;
         sock.connect(addr)?;
